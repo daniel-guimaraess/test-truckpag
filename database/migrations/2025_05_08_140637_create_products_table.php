f8598fbbe56c547c3504aa260f46813d34438a35
Daniel Guimarães <daniel_guimaraes2012@hotmail.com>
2025-05-08 11:25:14 -0300

    [FEATURE] - Create model and migration to product

---
+++ database/migrations/2025_05_08_140637_create_products_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('code');
+            $table->string('status');
+            $table->date('imported_t');
+            $table->string('url');
+            $table->string('creator');
+            $table->unsignedBigInteger('created_t')->nullable();
+            $table->unsignedBigInteger('last_modified_t')->nullable();
+            $table->string('product_name');
+            $table->string('quantity')->nullable();
+            $table->string('brands')->nullable();
+            $table->string('categories')->nullable();
+            $table->string('labels')->nullable();
+            $table->string('cities')->nullable();
+            $table->string('purchase_places')->nullable();
+            $table->string('stores')->nullable();
+            $table->string('ingredients_text')->nullable();
+            $table->string('traces')->nullable();
+            $table->string('serving_size')->nullable();
+            $table->float('serving_quantity')->nullable();
+            $table->integer('nutriscore_score')->nullable();
+            $table->string('nutriscore_grade')->nullable();
+            $table->string('main_category')->nullable();
+            $table->string('image_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
